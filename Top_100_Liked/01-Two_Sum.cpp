aadb6290cafc284ecd64ddaa50b00d50c22043ac
authetic <1019892846@qq.com>
2019-05-04 12:21:40 +0800

    Add Top100

---
+++ Top_100_Liked/01-Two_Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Description: 给出一个数组，和一个目标值，返回数组中两个数之和
+    等于目标值的对应下标
+    Key: 用undered_map存储，遍历数组。
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i ++ ) {
+            auto p = mp.find(target-nums[i]);
+            if (p != mp.end()) {
+                return {p->second, i};
+            }
+            mp[nums[i]] = i;
+        }
+        // 其实不需要这行语句，但leetcode编译通不过
+        return {0,1};
+    }
+};
